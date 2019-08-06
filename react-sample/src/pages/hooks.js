8770908ee75a6c86a55858ed7021e16fc995a895
jradek <jradek@yandex.com>
2019-08-06 00:46:49 +0000

    adds prettier (no semantic change)

--- react-sample/src/pages/hooks.js
+++ react-sample/src/pages/hooks.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
 
 const delayMilliSec = ms => new Promise(res => setTimeout(res, ms));
 
@@ -10,48 +9,78 @@ const BasicHooksExample = () => {
 
   return (
     <div className="row">
-      <div className="col s12"><h5>useState() hook example</h5></div>
-      <button className="col s4 waves-effect waves-light btn-small" onClick={() => setCounter(v => (v + 1))}>Increase</button>
+      <div className="col s12">
+        <h5>useState() hook example</h5>
+      </div>
+      <button
+        className="col s4 waves-effect waves-light btn-small"
+        onClick={() => setCounter(v => v + 1)}
+      >
+        Increase
+      </button>
       <div className="col s4 center">Value: {counter}</div>
-      <button className="col s4 waves-effect waves-light btn-small" onClick={() => setCounter(v => (v - 1))}>Decrease</button>
-    </div>)
-}
+      <button
+        className="col s4 waves-effect waves-light btn-small"
+        onClick={() => setCounter(v => v - 1)}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
 
 // ****************************************************************************
 
 // this is a generic form hook, which could be applied to any form
-const useFormHook = (initialValues) => {
-  const [values, setValues] = useState(initialValues)
+const useFormHook = initialValues => {
+  const [values, setValues] = useState(initialValues);
 
   return [
-    values, e => {
+    values,
+    e => {
       setValues({
         // keep old values
         ...values,
         // we update its value based on then 'name' property of the input field
         [e.target.name]: e.target.value
       });
-      console.log("useFormHook, updated ", e.target.name)
+      console.log("useFormHook, updated ", e.target.name);
     }
-  ]
-}
+  ];
+};
 
 const FormHooksExample = () => {
   const [values, handleChange] = useFormHook({ email: "", password: "" });
 
   return (
     <div className="row">
-      <div className="col s12"><h5>custom hooks</h5></div>
-      <div className="input-field col s12 m6">
-        <input placeholder="email" name="email" type="text" value={values.email} onChange={handleChange} />
+      <div className="col s12">
+        <h5>custom hooks</h5>
       </div>
       <div className="input-field col s12 m6">
-        <input placeholder="password" name="password" type="password" value={values.password} onChange={handleChange} />
+        <input
+          placeholder="email"
+          name="email"
+          type="text"
+          value={values.email}
+          onChange={handleChange}
+        />
       </div>
-      <div className="col s12 center">{values.email}, {values.password}</div>
+      <div className="input-field col s12 m6">
+        <input
+          placeholder="password"
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="col s12 center">
+        {values.email}, {values.password}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 // ****************************************************************************
 // see: https://www.youtube.com/watch?v=f687hBjwFcM&list=PL9VMWoQ0LdqGT_Ql-ojVrreXrlVRvutIZ&index=2&t=1167s
@@ -60,24 +89,24 @@ const FormHooksExample = () => {
 const useFetch = ({ url, delayMSec = 500 }) => {
   const [state, setState] = useState({ data: null, isLoading: true });
 
-  useEffect(() => {
-    setState({ data: null, isLoading: true });
+  useEffect(
+    () => {
+      setState({ data: null, isLoading: true });
 
-    delayMilliSec(delayMSec)
-      .then(() => {
+      delayMilliSec(delayMSec).then(() => {
         fetch(url)
           .then(x => x.text())
           .then(y => {
-            setState({ data: y, isLoading: false })
-          })
+            setState({ data: y, isLoading: false });
+          });
       });
-  },
+    },
     // effect is triggered whenever the url changes or the delay
     [url, delayMSec]
   );
 
   return state;
-}
+};
 
 const EffectHookExample = () => {
   const [number, setNumber] = useState(0);
@@ -85,28 +114,33 @@ const EffectHookExample = () => {
   // destructuring with renaming:
   // rename the generic 'data' to numberTrivia
   // see: https://wesbos.com/destructuring-renaming/
-  const { data: numberTrivia, isLoading } = useFetch(
-    { url: `http://numbersapi.com/${number}/trivia` });
+  const { data: numberTrivia, isLoading } = useFetch({
+    url: `http://numbersapi.com/${number}/trivia`
+  });
 
   return (
     <div className="row">
       <div className="col s12">
         <h5>useEffect()</h5>
       </div>
-      <button className="col s2 waves-effect waves-light btn-small"
+      <button
+        className="col s2 waves-effect waves-light btn-small"
         onClick={() => {
           // a button press changes the number
           //  which changes the URL
           //  which triggers the effect
-          setNumber(n => n + 1)
-        }}>Next</button>
-      <div className="col s2"><h6>{number}</h6></div>
-      <div className="col s8">
-        {isLoading ? "Loading ..." : numberTrivia}
+          setNumber(n => n + 1);
+        }}
+      >
+        Next
+      </button>
+      <div className="col s2">
+        <h6>{number}</h6>
       </div>
+      <div className="col s8">{isLoading ? "Loading ..." : numberTrivia}</div>
     </div>
-  )
-}
+  );
+};
 
 // ****************************************************************************
 
@@ -115,13 +149,20 @@ const Hooks = () => {
     // <div>
     <div>
       <h2>Hooks</h2>
-      <div className="row"><div className="col s12"><p>The contents of this site is based on this <a href="https://www.youtube.com/watch?v=f687hBjwFcM">tutorial</a></p></div></div>
+      <div className="row">
+        <div className="col s12">
+          <p>
+            The contents of this site is based on this{" "}
+            <a href="https://www.youtube.com/watch?v=f687hBjwFcM">tutorial</a>
+          </p>
+        </div>
+      </div>
       <BasicHooksExample />
       <FormHooksExample />
       <EffectHookExample />
     </div>
     // </div>
-  )
-}
+  );
+};
 
 export default Hooks;
